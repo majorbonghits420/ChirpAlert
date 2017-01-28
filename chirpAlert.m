62d7d760e3247b82faa67568682008f2deb621c1
majorbonghits420 <jcc384@cornell.edu>
2017-01-28 14:09:44 -0500

    Skeleton Outline for Code
    Created a skeleton file which maps out some of the high level
    functions that are going to be written. It is expected that when
    writing for one of these functions that you divide necessary
    tasks into smaller subfunctions if it gets too big. That
    function should be defined right below the higher level
    function. Currently each function takes no inputs and gives no
    outputs, modify these as needed. Update comments so that it
    accurately documents what each function does and how. DO NOT USE
    TABS, DO NOT LEAVE TRAILING WHITESPACES.

---
+++ chirpAlert.m
@@ -0,0 +1,81 @@
+% ChirpAlert.m
+% Authors: Suzie, James, Thadd, Mary, Sooyan
+% Date: 28 Jan 17
+% Description: TODO
+
+
+% ChirpAlert()
+% Inputs:
+% Outputs:
+% Description
+function [] = chirpAlert()
+% Setup Global
+
+% Setup Audio
+
+% Setup ML stuff
+
+% Buffer update
+
+% Detection
+
+% Interpret results
+
+% Produce outputs to user
+end
+
+% setupGlobalvars()
+% Inputs:
+% Outputs:
+% Description
+function [] = setupGlobalVars()
+
+end
+
+% setupAudio()
+% Inputs:
+% Outputs:
+% Description
+function [] = setupAudio()
+
+end
+
+% setupML()
+% Inputs:
+% Outputs:
+% Description
+function [] = setupML()
+
+end
+
+% updateBuffer()
+% Inputs:
+% Outputs:
+% Description:
+function [] = updateBuffer()
+
+end
+
+% detection()
+% Inputs:
+% Outputs:
+% Description:
+function [] = detection()
+
+end
+
+% interpretresults()
+% Inputs:
+% Outputs:
+% Description:
+function [] = interpretResults()
+
+end
+
+% outputUserresults()
+% Inputs:
+% Outputs:
+% Description:
+function [] = outputUserResults()
+
+end
