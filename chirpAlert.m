e40c3f4fbebca3a83b235a183a2e31a029aec775
majorbonghits420 <jcc384@cornell.edu>
2017-01-28 15:57:55 -0500

    Added buffer, counters, and params
    Added necessary parameters for the buffer and where we are
    reading the audio from. Created the global variables for the
    audio buffer and counters for number of coughs and chirps

--- chirpAlert.m
+++ chirpAlert.m
@@ -25,11 +25,33 @@ function [] = chirpAlert()
 end
 
 % setupGlobalvars()
-% Inputs:
-% Outputs:
-% Description
+% Inputs: None
+% Outputs: None
+% Description: This function setups up the various global variables that will
+% be used throught the program
 function [] = setupGlobalVars()
+  setGlobalparams();
+  % Create a buffer for the audio
+  global audioBuffer = zeros(1,BUFFER_SIZE);
+  % Counter for number of chirps
+  global numChirps = 0;
+  % Counter for number of abnormalities
+  global numAbnormal = 0;
+end
 
+% setupGlobalparams()
+% Inputs: None
+% Outputs: None
+% Description: Used to set parameters for the rest of the program
+function [] = setGlobalParams()
+  % Amount of time present in the bufer
+  global BUFFER_TIME = 1; % Guess value, need to change
+  % The audio sampling rate
+  global SAMPLE_RATE = 44100; % 44.1 kHz since that is standard sampling rate
+  % How many data points are in our buffer (floored for integer number)
+  global BUFFER_SIZE = floor(SAMPLE_RATE / BUFFER_TIME);
+  % Filename to get audio from
+  global FILE_NAME = "";
 end
 
 % setupAudio()
